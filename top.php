edc0e20dc4b4cbb8f172e59cce1d6e7d54a80f35
riazbm <rizikimohamed968@gmail.com>
2023-03-16 19:14:13 -0400

    second commit

---
+++ top.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<?php include 'top.php';?>
+    <h1>welcome to ai car wash</h1>
+    <p>we are fast and ready any time you would wish us to help you</p>
+    <a href="/default.php">Home</a>
+    <a href="/products.php">Products</a>
+    <a href="/services.php">Services</a>
+</body>
+</html>
